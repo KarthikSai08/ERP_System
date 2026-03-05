602077fde3baf6108282318c108657f4add2d86b
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-03-05 16:44:22 +0530

    chore: initialize solution with Clean Architecture project structure

---
+++ ERP_System.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERP_System.Domain.Entities
+{
+    internal class BaseEntity
+    {
+    }
+}
---
+++ ERP_System.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERP_System.Domain.Exceptions
+{
+    internal class DomainException
+    {
+    }
+}
