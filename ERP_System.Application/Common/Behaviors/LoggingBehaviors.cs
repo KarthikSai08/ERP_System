adbbf054ec95627595c51d94597d55da3d158944
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-03-09 13:26:58 +0530

    feat(application): add loggingBehavior and validationBehavior

---
+++ ERP_System.Application/Common/Behaviors/LoggingBehaviors.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERP_System.Application.Common.Behaviors
+{
+    public class LoggingBehaviors<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        private readonly ILogger<LoggingBehaviors<TRequest, TResponse>> _logger;
+        public LoggingBehaviors(ILogger<LoggingBehaviors<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request,
+                                            RequestHandlerDelegate<TResponse> next,
+                                            CancellationToken cancellationToken)
+        {
+            var reqName = typeof(TRequest).Name;
+            _logger.LogInformation("-> Handling {reqName}", reqName);
+
+            var response = await next();
+            _logger.LogInformation("Handled {reqName}", reqName);
+
+            return response;
+        }
+    }
+}
---
+++ ERP_System.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,52 @@
+﻿using FluentValidation;
+using ERP_System.Domain.Exceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ValidationException = ERP_System.Domain.Exceptions.ValidationException;
+
+namespace ERP_System.Application.Common.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+                                                            where TRequest : notnull
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+            => _validators = validators;
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken)
+        {
+            if (!_validators.Any())
+                return await next();
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var results = await Task.WhenAll(
+                _validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+            var failures = results
+                .SelectMany(r => r.Errors)
+                .Where(f => f is not null)
+                .ToList();
+
+            if (failures.Any())
+            {
+                var errors = failures
+                    .GroupBy(f => f.PropertyName)
+                    .ToDictionary(
+                    g => g.Key,
+                    g => g.Select(f => f.ErrorMessage).ToArray());
+
+                throw new ValidationException(errors);
+            }
+            return await next();
+
+        }
+
+    }
+}
