bbd1a572e9198c2871196afc0f1806addeca58a5
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-03-17 14:05:04 +0530

    feat(inventory) : add GetLowStockAlertQuery & GetLowStockAlertQueryHandler

---
+++ ERP_System.Application/Features/Inventory/Query/GetLowStockAlertQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using ERP_System.Application.Common;
+using ERP_System.Application.DTOs;
+using ERP_System.Domain.Interfaces;
+using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ERP_System.Application.Features.Inventory.Query
+{
+    public class GetLowStockAlertQueryHandler : IRequestHandler<GetLowstockAlertQuery, ApiResponse<IEnumerable<LowStockAlertResponse>>>
+    {
+        private readonly IStockRepository _stkRepo;
+        private readonly IMapper _mapper;
+
+        public GetLowStockAlertQueryHandler(IStockRepository stkRepo, IMapper mapper)
+        {
+            _stkRepo = stkRepo;
+            _mapper = mapper;
+        }
+        public async Task<ApiResponse<IEnumerable<LowStockAlertResponse>>> Handle(GetLowstockAlertQuery qry, CancellationToken ct)
+        {
+            var stock = await _stkRepo.GetLowStockItemsAsync(ct);
+            var res = _mapper.Map<IEnumerable<LowStockAlertResponse>>(stock);
+
+            return ApiResponse<IEnumerable<LowStockAlertResponse>>.Ok(res);
+        }
+    }
+}
---
+++ ERP_System.Application/Features/Inventory/Query/GetLowstockAlertQuery.cs
@@ -0,0 +1,12 @@
+﻿using ERP_System.Application.Common;
+using ERP_System.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERP_System.Application.Features.Inventory.Query
+{
+    public record GetLowstockAlertQuery : IRequest<ApiResponse<IEnumerable<LowStockAlertResponse>>>;
+   
+}
