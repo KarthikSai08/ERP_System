4e40d99a8162c455cf81774246b411929f02ae3b
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-03-18 17:50:41 +0530

    feat(Repositories) : refact ProductRepository with Dapper(read) & EFCore(write)

--- ERP_System.Infrastructure/Persistence/Repositories/ProductRepository.cs
+++ ERP_System.Infrastructure/Persistence/Repositories/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using ERP_System.Domain.Entities;
+﻿using Dapper;
+using ERP_System.Domain.Entities;
 using ERP_System.Domain.Interfaces;
 using ERP_System.Infrastructure.Persistence.Context;
 using System;
@@ -11,11 +12,13 @@ namespace ERP_System.Infrastructure.Persistence.Repositories
     {
         private readonly AppDbContext _context;
         private readonly string _conn;
+        private readonly DapperContext _dapperContext;
 
-        public ProductRepository(AppDbContext context, string conn)
+        public ProductRepository(AppDbContext context, string conn,DapperContext dapperContext)
         {
             _context = context;
             _conn = conn;
+            _dapperContext = dapperContext;
         }
 
         public async Task<int> AddAsync(Product product,CancellationToken ct)
@@ -26,34 +29,111 @@ namespace ERP_System.Infrastructure.Persistence.Repositories
 
         }
 
-        public Task DeleteAsync(int id,CancellationToken ct)
+        public async Task DeleteAsync(int id,CancellationToken ct)
         {
-            throw new NotImplementedException();
+            var p = await _context.Products.FindAsync(id);
+            if(p != null)
+            {
+                _context.Products.Remove(p);
+                await _context.SaveChangesAsync(ct);
+            }
         }
 
-        public Task<IEnumerable<Product>> GetAllAsync(CancellationToken ct)
+        public async Task<IEnumerable<Product>> GetAllAsync(CancellationToken ct)
         {
-            throw new NotImplementedException();
+            using var con = _dapperContext.CreateConnection();
+
+            var sql = @"SELECT p.*, c.CategoryName, c.Description, c.IsActive
+                        FROM Products p
+                        LEFT JOIN Categories c On p.CategoryId = c.CategoryId
+                        WHERE  p.IsActive = 1
+                        ORDER BY p.ProductName";
+
+            var result = await con.QueryAsync<Product, Category, Product>(
+                sql,
+                (product, cat) =>
+                {
+                    product.SetCategory(cat);
+                    return product;
+
+                },
+                splitOn: "ProductId");
+
+            return result;
         }
 
-        public Task<Product?> GetByIdAsync(int id,CancellationToken ct  )
+        public async Task<Product?> GetByIdAsync(int id,CancellationToken ct)
         {
-            throw new NotImplementedException();
+            using var con = _dapperContext.CreateConnection();
+            var sql = @"SELECT p.*, c.CategoryName, c.Description, c.IsActive
+                        FROM Products p
+                        LEFT JOIN Categories c On p.CategoryId = c.CategoryId
+                        WHERE p.ProductId = @Id";
+
+            var result = await con.QueryAsync<Product, Category, Product>(
+                sql,
+                (product, cat) =>
+                {
+                    product.SetCategory(cat);
+                    return product;
+                },
+                new { Id = id },
+                splitOn: "ProductId");
+
+            return result.FirstOrDefault();
+                        
         }
 
-        public Task<IEnumerable<Product>> SearchAsync(string? name, int? categoryId, decimal? maxPrice,CancellationToken ct)
+        public async Task<IEnumerable<Product>> SearchAsync(string? name, int? categoryId, decimal? maxPrice,CancellationToken ct)
         {
-            throw new NotImplementedException();
+            using var con = _dapperContext.CreateConnection();
+            var sql = new StringBuilder(@"SELESELECT p.*, c.CategoryName, c.Description, c.IsActive
+                        FROM Products p
+                        LEFT JOIN Categories c On p.CategoryId = c.CategoryId
+                        WHERE p.IsActive = 1");
+
+            var param = new DynamicParameters();
+
+            if(!string.IsNullOrEmpty(name))
+            {
+                sql.Append(" AND p.ProductName Like @Name");
+                param.Add("Name", $"%{name}%");
+            }
+            if (categoryId.HasValue)
+            {
+                sql.Append(" AND p.CategoryId = @CategoryId");
+                param.Add("CategoryId", categoryId.Value);
+            }
+            if (maxPrice.HasValue)
+            {
+                sql.Append(" AND p.Price <= @MaxPrice");
+                param.Add("MaxPrice",maxPrice.Value);
+            }
+            sql.Append(" ORDER BY p.ProductName");
+
+            return await con.QueryAsync<Product, Category, Product>(
+                sql.ToString(),
+                (prd, cat) =>
+                {
+                    prd.SetCategory(cat);
+                    return prd;
+                },
+                splitOn: "ProductId");
         }
 
-        public Task<bool> SkuExistAsync(string sku, CancellationToken ct)
+        public async Task<bool> SkuExistAsync(string sku, CancellationToken ct)
         {
-            throw new NotImplementedException();
+            using var con = _dapperContext.CreateConnection();
+
+            return await con.QuerySingleOrDefaultAsync<bool>(
+                "SELECT CAST(CASE WHEN EXISTS(SELECT 1 FROM Products WHERE SKU =@SKU) THEN 1 ELSE 0 END AS BIT)",
+                new { SKU = sku.ToUpper() });
         }
 
-        public Task UpdateAsync(Product product, CancellationToken ct)
+        public async Task UpdateAsync(Product product, CancellationToken ct)
         {
-            throw new NotImplementedException();
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
         }
     }
 }
