c74943e9a7ed4d36490b4ddd16a8f63b09273ce5
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-03-18 13:38:01 +0530

    chore : setup DependencyInjection on Application & Infrastucture

---
+++ ERP_System.Application/DependencyInjection/ApplicationServices.cs
@@ -0,0 +1,26 @@
+﻿using ERP_System.Application.Common.Behaviors;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ERP_System.Application.DependencyInjection
+{
+    public static class ApplicationServices
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assembly));
+
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviors<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
---
+++ ERP_System.Infrastructure/DependencyInjection/InfrastructureServices.cs
@@ -0,0 +1,35 @@
+﻿using ERP_System.Domain.Interfaces;
+using ERP_System.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace ERP_System.Infrastructure.DependencyInjection
+{
+    public static class InfrastructureServices
+    {
+
+        public static IServiceCollection AddInfrastucture(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppDbContext>(opts =>
+                opts.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IStockRepository, StockRepository>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<ISupplierRepository, SupplierRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
