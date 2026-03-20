ed90fc635e1ab8df19716f91a2beddd56976ac93
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-03-20 15:38:15 +0530

    feat(Repositories) : add SupplierRepository(Ef-Core only)

---
+++ ERP_System.Infrastructure/Persistence/Repositories/SupplierRepository.cs
@@ -0,0 +1,42 @@
+﻿using ERP_System.Domain.Entities;
+using ERP_System.Domain.Interfaces;
+using ERP_System.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERP_System.Infrastructure.Persistence.Repositories
+{
+    public class SupplierRepository : ISupplierRepository
+    {
+        private readonly AppDbContext _context;
+
+        public SupplierRepository(AppDbContext context)
+            => _context = context;
+
+        public async Task<int> AddAsync(Supplier supplier, CancellationToken ct)
+        {
+            _context.Suppliers.Add(supplier);
+            await _context.SaveChangesAsync();
+            return supplier.SupplierId;
+        }
+
+        public async Task<bool> EmailExistsAsync(string email, CancellationToken ct)
+            => await _context.Suppliers.AnyAsync(s => s.Email == email, ct);
+
+        public async Task<IEnumerable<Supplier>> GetAllAsync(CancellationToken ct)
+            =>  await _context.Suppliers.AsNoTracking().ToListAsync();
+
+
+        public async Task<Supplier?> GetByIdAsync(int id, CancellationToken ct)
+            =>await _context.Suppliers.FirstOrDefaultAsync(s => s.SupplierId == id);
+
+        public async Task UpdateAsync(Supplier supplier, CancellationToken ct)
+        {
+            _context.Suppliers.Update(supplier);
+            await _context.SaveChangesAsync(ct);
+
+        }
+    }
+}
